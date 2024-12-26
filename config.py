0539afd9833341eb098373dac0b94e5a61a0b8aa
PAWAN BISHNOI <knowledgetechnology3@gmail.com>
2024-12-26 18:12:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("21174134", ))
+    API_ID = int(os.getenv(21174134, ))
     API_HASH = os.getenv("3d7f8c4ee4978228f411bbd04f2eea73", '')
     BOT_TOKEN = os.getenv("7168358367:AAH21vD3sNQIMiV_Qg9NfG0VO1OI81-hdec", '')
